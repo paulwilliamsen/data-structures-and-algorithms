93f4e80e28c313d7c5bd7fb1fe687b3a99b216fb
Paul Williamsen <pwilliamsen@me.com>
2019-01-23 08:43:11 -0800

    question 4 done

--- code challanges/301/2array/challenges-12.test.js
+++ code challanges/301/2array/challenges-12.test.js
@@ -23,7 +23,7 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
+ const
 
 }
 
